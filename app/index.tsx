339d8158f035cc188f7c3de1a49be0dcfe66928f
MrBarker47 <jebarker7@gmail.com>
2025-08-22 01:15:12 -0400

    Update files

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>Go to About Screen </Link>
     </View>
   );
 }
@@ -18,4 +20,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#ffffffff',
   },
+  button: {
+    fontSize: 28,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 });
+
