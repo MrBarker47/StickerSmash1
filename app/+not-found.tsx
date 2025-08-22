e901a99f707c76008b46c31d73cd271b88578f32
MrBarker47 <jebarker7@gmail.com>
2025-08-22 19:30:38 -0400

    Update files

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,15 +1,15 @@
-import { View, StyleSheet } from 'react-native';
-import { Link, Stack} from 'expo-router';
+import { View, StyleSheet} from 'react-native';
+import { Link, Stack } from 'expo-router';
 
 export default function NotFoundScreen() {
     return (
         <>
         <Stack.Screen options={{title: 'Opps! Not Found'}} />
         <View style={styles.container}>
-        <Link href="/" style={styles.button}>
-            Go back to Home Screen!
-            </Link>
-          </View>
+        <Link href="/home" style= {styles.button}>
+        Go Back to home Screen
+        </Link>
+        </View>
         </>
     );
 }
